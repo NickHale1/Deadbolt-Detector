ef4b80b53469f7c2c380bfab8c331b7660cb0e04
NickHale1 <47830428+NickHale1@users.noreply.github.com>
2022-06-21 00:30:48 -0400

    Implemented login/Signup
    implemented login/signup

--- App/deadbolt-detector/deadbolt-detector/deadbolt_detectorApp.swift
+++ App/deadbolt-detector/deadbolt-detector/deadbolt_detectorApp.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct deadbolt_detectorApp: App {
-    let persistenceController = PersistenceController.shared
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
+            let viewModel = AppViewModel()
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(viewModel)
         }
     }
 }
+
+
+class AppDelegate:NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
