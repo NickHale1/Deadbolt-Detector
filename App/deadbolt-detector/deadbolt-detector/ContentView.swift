8306df0234ebfb94d6a86934e2a90567d38ba23c
NickHale1 <47830428+NickHale1@users.noreply.github.com>
2022-06-28 13:15:01 -0400

    updates for sprint 2 presentation

--- App/deadbolt-detector/deadbolt-detector/ContentView.swift
+++ App/deadbolt-detector/deadbolt-detector/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import CoreData
 import FirebaseAuth
+import Foundation
 
 
 class AppViewModel: ObservableObject{
@@ -24,15 +25,27 @@ class AppViewModel: ObservableObject{
         auth.signIn(withEmail: email, password: password) { [weak self]
             result, error in
             guard result != nil, error == nil else {
+
                 return
             }
             //success
             DispatchQueue.main.async {
                 self?.signedIn = true
+
             }
 
         }
     }
+    
+    func resetPassword(email: String){
+        auth.sendPasswordReset(withEmail: email){ (error) in
+            if let error=error {
+                return
+            }
+     
+        }
+    }
+     
     
     func signUp(email: String, password: String){
         auth.createUser(withEmail: email, password: password) { [weak self] result, error in
@@ -56,14 +69,78 @@ class AppViewModel: ObservableObject{
     
 }
 
+class APIRequestManager: ObservableObject {
+    private let key = "54324"
+    
+    @Published var result = "Unlocked"
+    @Published var resultBool=false
+    @Published var inputURL = "https://deadbolt-detector-default-rtdb.firebaseio.com/Detectors/12345.json"
+    
+    func getData() {
+        guard let url = URL(string: inputURL) else {
+            print("invaludURL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) {
+            (data,response,error) in
+            guard let data = data else {
+                print("could not get data")
+                DispatchQueue.main.async {
+                    self.result = "could not get data"
+                }
+                return
+            }
+            do {
+                let myresult = try JSONDecoder().decode(detector.self, from:data)
+                DispatchQueue.main.async {
+                    print(myresult)
+                    if(myresult.status==true){
+                        self.result="Locked"
+                    } else {
+                        self.result="Unlocked"
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    print("\(error)")
+                }
+            }
+            
+        }
+        .resume()
+    }
+}
+
+func getStatus() -> String {
+    return "Beans";
+}
+
+struct detector: Codable {
+    let status:Bool
+}
+
 struct ContentView: View {
+    
+    let url = "https://deadbolt-detector-default-rtdb.firebaseio.com/Detectors/12345.json"
+    @StateObject var apiMan = APIRequestManager()
     @EnvironmentObject var viewModel: AppViewModel
+    @State var status = "Locked"
     
     var body: some View {
         NavigationView{
             if viewModel.signedIn {
                 VStack{
-                    Text("You are Signed in").padding()
+                    Section("My Lock") {
+                        Text(apiMan.result)
+                        Button(action: {
+                            apiMan.result="Checking your lock..."
+                            apiMan.getData()
+                        }, label: {
+                            Text("Update").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white).padding()
+                        })
+                    
+                    }
                     Button(action: {
                         viewModel.signOut()
                     }, label: {
@@ -76,6 +153,46 @@ struct ContentView: View {
         }.onAppear{
             viewModel.signedIn=viewModel.isSignedIn
         }
+    }
+}
+
+struct ResetPasswordView: View {
+    
+    @State var email=""
+    @State var password=""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        
+        VStack{
+            Image("Logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150,height: 150)
+            VStack {
+                TextField("Email Address", text:$email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                Text("").padding()
+                Button(action: {
+                    guard !email.isEmpty else{
+                        return
+                    }
+                    
+                    viewModel.resetPassword(email: email)
+                }, label: {
+                    Text("Reset Password").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white)
+                })
+                
+            }.padding()
+            
+            Spacer()
+            
+        }.navigationTitle("Reset Password")
+        
     }
 }
 
@@ -111,12 +228,14 @@ struct SignInView: View {
                         return
                     }
                     
-                    viewModel.signIn(email: email, password: password)
+                   viewModel.signIn(email: email, password: password)
+                  //  print(result)
                 }, label: {
                     Text("Sign In").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white)
                 })
                 
                 NavigationLink("Create Account", destination: SignUpView()).padding()
+                NavigationLink("Reset Password", destination: ResetPasswordView()).padding()
                 
             }.padding()
             
