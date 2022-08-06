42746b5efe0e521cd53ab1840b513c714a1180c0
NickHale1 <47830428+NickHale1@users.noreply.github.com>
2022-08-06 13:05:48 -0400

    moved some files to the cloud on my laptop so had to update some files

--- App/deadbolt-detector/deadbolt-detector/ContentView.swift
+++ App/deadbolt-detector/deadbolt-detector/ContentView.swift
@@ -10,17 +10,16 @@ import CoreData
 import FirebaseAuth
 import Foundation
 
-
+// Class that contains the firebase authentication functions
 class AppViewModel: ObservableObject{
     
     let auth = Auth.auth()
-    
     @Published var signedIn = false
-    
     var isSignedIn: Bool {
         return auth.currentUser != nil
     }
     
+    // Sign in function
     func signIn(email: String, password: String){
         auth.signIn(withEmail: email, password: password) { [weak self]
             result, error in
@@ -36,7 +35,7 @@ class AppViewModel: ObservableObject{
 
         }
     }
-    
+    // Reset password function
     func resetPassword(email: String){
         auth.sendPasswordReset(withEmail: email){ (error) in
             if let error=error {
@@ -46,7 +45,7 @@ class AppViewModel: ObservableObject{
         }
     }
      
-    
+    // Signup function
     func signUp(email: String, password: String){
         auth.createUser(withEmail: email, password: password) { [weak self] result, error in
             guard result != nil, error == nil else {
@@ -60,7 +59,7 @@ class AppViewModel: ObservableObject{
         }
         
     }
-    
+    // Sign out function
     func signOut(){
         try? auth.signOut()
         self.signedIn=false
@@ -69,6 +68,7 @@ class AppViewModel: ObservableObject{
     
 }
 
+// Class to handle a lot of the API requests for views
 class APIRequestManager: ObservableObject {
     private let key = "54324"
     
@@ -77,7 +77,7 @@ class APIRequestManager: ObservableObject {
     @Published var resultBool=false
     @Published var imageName = "lock.open.fill"
     @Published var inputURL = "https://deadbolt-detector-default-rtdb.firebaseio.com/Detectors/12345.json"
-    
+    // Get the detector status of the user's device
     func getData() {
         guard let url = URL(string: inputURL) else {
             print("invaludURL")
@@ -98,9 +98,11 @@ class APIRequestManager: ObservableObject {
                 DispatchQueue.main.async {
                     print(myresult)
                     if(myresult.status==true){
+                        // Door is locked
                         self.result="Locked"
                         self.imageName="lock.fill"
                     } else {
+                        // Door is not locked
                         self.result="Unlocked"
                         self.imageName="lock.open.fill"
                     }
@@ -116,13 +118,11 @@ class APIRequestManager: ObservableObject {
     }
 }
 
-func getStatus() -> String {
-    return "Beans";
-}
-
+// Basic data structure to store the boolean value from the API
 struct detector: Codable {
     let status:Bool
 }
+// The Request location view
 struct RequestLocationView: View {
     var body: some View {
         ZStack{
@@ -150,61 +150,49 @@ struct RequestLocationView: View {
         
     }
 }
-
+//Main view - Contains logic for sign in view and the functionality for the home screen
 struct ContentView: View {
     @ObservedObject var locationManager = LocationManager.shared
-    
     let url = "https://deadbolt-detector-default-rtdb.firebaseio.com/Detectors/12345.json"
     @StateObject var apiMan = APIRequestManager()
-   
     @EnvironmentObject var viewModel: AppViewModel
-    //@Published var test = "Logo"
     @State var status = "Locked"
     
     var body: some View {
        
         NavigationView{
+            // If the user is signed in
             if viewModel.signedIn {
+                // If the user has not allowed location services
                 if(locationManager.userLocation==nil){
+                    // Show the view asking for location permissions
                     RequestLocationView()
                 } else {
-                    
+                    //The home screen
                 
                 VStack{
                     Section() {
+                        // Lock image
                         Image(systemName: apiMan.imageName)
                             .resizable()
                             .scaledToFit()
                             .frame(width: 150,height: 150)
                             .padding(.bottom, 40)
-                        
+                        // Update button
                         Text("Update")
                             .font(.system(size:24))
                         Button(action: {
                             apiMan.result="Checking your lock..."
                             apiMan.getData()
-                            
-                            print(LocationManager.shared.userLocation)
-                            /*
-                            let content = UNMutableNotificationContent()
-                            content.title = "Feed the cat"
-                            content.subtitle = "It looks hungry"
-                            content.sound = UNNotificationSound.default
-
-                            // show this notification five seconds from now
-                            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                            // choose a random identifier
-                            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                            // add our notification request
-                            UNUserNotificationCenter.current().add(request)
-                             */
+                            // Debug statement to print the user's location
+                            // print(LocationManager.shared.userLocation)
+                     
                         }, label: {
                             Text("Update").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white).padding()
                         })
                     
                     }
+                    // Sign Out button
                     Button(action: {
                         viewModel.signOut()
                     }, label: {
@@ -212,15 +200,18 @@ struct ContentView: View {
                     })
                 }
                 }
+                // If not signed in
             } else {
+                // Show the Sign In view
                 SignInView()
             }
         }.onAppear{
+            //checks the signed in status
             viewModel.signedIn=viewModel.isSignedIn
         }
     }
 }
-
+// Reset password view
 struct ResetPasswordView: View {
     
     @State var email=""
@@ -231,6 +222,7 @@ struct ResetPasswordView: View {
     var body: some View {
         
         VStack{
+            // Logo
             Image(systemName: "lock.shield")
                 .resizable()
                 .scaledToFit()
@@ -238,17 +230,20 @@ struct ResetPasswordView: View {
                 .padding(.bottom,40)
                 .padding(.top,40)
             VStack {
+                // Email text box
                 TextField("Email Address", text:$email)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
                     .padding()
                     .background(Color(.secondarySystemBackground))
                 Text("").padding()
+                // Submit password reset request
                 Button(action: {
                     guard !email.isEmpty else{
+                        // TODO: update the text to say please enter an email
                         return
                     }
-                    
+                    // TODO: Update the text field to say request sent
                     viewModel.resetPassword(email: email)
                 }, label: {
                     Text("Reset Password").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white)
@@ -263,7 +258,7 @@ struct ResetPasswordView: View {
     }
 }
 
-
+// Sign In View
 struct SignInView: View {
     
     @State var email=""
@@ -274,6 +269,7 @@ struct SignInView: View {
     var body: some View {
         
         VStack{
+            // Logo image
             Image(systemName: "lock.shield")
                 .resizable()
                 .scaledToFit()
@@ -281,29 +277,32 @@ struct SignInView: View {
                 .padding(.bottom,40)
                 .padding(.top,40)
             VStack {
+                // Email text field
                 TextField("Email Address", text:$email)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
                     .padding()
                     .background(Color(.secondarySystemBackground))
+                // Password text field
                 SecureField("Password", text:$password)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
                     .padding()
                     .background(Color(.secondarySystemBackground))
-                
+                // Submit Button
                 Button(action: {
                     guard !email.isEmpty, !password.isEmpty else {
                         return
                     }
-                    
+                    // TODO: update a text field for error messages
                    viewModel.signIn(email: email, password: password)
                   //  print(result)
                 }, label: {
                     Text("Sign In").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white)
                 })
-                
+                // Link to create account page
                 NavigationLink("Create Account", destination: SignUpView()).padding()
+                // Link to reset password page
                 NavigationLink("Reset Password", destination: ResetPasswordView()).padding()
                 
             }.padding()
@@ -315,19 +314,7 @@ struct SignInView: View {
     }
 }
 
-struct MainMenuView: View {
-    var body: some View{
-        VStack{
-            Text("You are Signed in")
-            Button(action: {
-                //viewModel.out()
-            }, label: {
-                Text("Sign Out").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white)
-            })
-        }
-    }
-}
-
+// Sign Up View
 struct SignUpView: View {
     
     @State var email=""
@@ -338,6 +325,7 @@ struct SignUpView: View {
     var body: some View {
         
         VStack{
+            // Logo image
             Image(systemName: "lock.shield")
                 .resizable()
                 .scaledToFit()
@@ -345,22 +333,26 @@ struct SignUpView: View {
                 .padding(.bottom,40)
                 .padding(.top,40)
             VStack {
+                // Email text field
                 TextField("Email Address", text:$email)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
                     .padding()
                     .background(Color(.secondarySystemBackground))
+                // Password text field
                 SecureField("Password", text:$password)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
                     .padding()
                     .background(Color(.secondarySystemBackground))
                 
+                // submit button
                 Button(action: {
                     guard !email.isEmpty, !password.isEmpty else {
                         return
                     }
                     
+                    // TODO: error handling
                     viewModel.signUp(email: email, password: password)
                 }, label: {
                     Text("Create Account").frame(width: 200, height: 50).background(Color.blue).cornerRadius(8).foregroundColor(Color.white)
@@ -374,8 +366,6 @@ struct SignUpView: View {
         
     }
 }
-
-
 
 
 private let itemFormatter: DateFormatter = {
--- App/deadbolt-detector/deadbolt-detector/LocationManager.swift
+++ App/deadbolt-detector/deadbolt-detector/LocationManager.swift
@@ -8,6 +8,7 @@
 import CoreLocation
 import UserNotifications
 
+//Location Manager allows us to get access to the user's location
 class LocationManager: NSObject, ObservableObject {
     private var homeLocation: CLLocation!
     private let manager = CLLocationManager()
@@ -27,6 +28,7 @@ class LocationManager: NSObject, ObservableObject {
     }
 }
 
+//print the location authorization status when it changes
 extension LocationManager: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         
@@ -51,8 +53,6 @@ extension LocationManager: CLLocationManagerDelegate {
             break
 
         }
-        
-        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -66,8 +66,10 @@ extension LocationManager: CLLocationManagerDelegate {
         
         let distanceThreshold = 20.0 // meters
         
+        //if you are more than 20 meters from the home location and this is the first time you've breached the barrier since you've left
         if(location.distance(from: homeLocation) > distanceThreshold && !alreadyBreached){
             
+            //make an API request to the detector
             let inputURL = "https://deadbolt-detector-default-rtdb.firebaseio.com/Detectors/12345.json"
             guard let url = URL(string: inputURL) else {
                 print("invaludURL")
@@ -87,22 +89,16 @@ extension LocationManager: CLLocationManagerDelegate {
                     DispatchQueue.main.async {
                         print(myresult)
                         if(myresult.status==true){
-                            //the door is locked
-                         
+                            //the door is locked, do nothing
                         }else {
                             //the door is unlocked
+                            //Send a notification to display in 5 seconds
                             let content = UNMutableNotificationContent()
                             content.title = "Did you forget to lock your door?"
                             content.body = "It looks like you forgot to lock your door on your way out"
                             content.sound = UNNotificationSound.default
-
-                            // show this notification five seconds from now
                             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                            // choose a random identifier
                             let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                            // add our notification request
                             UNUserNotificationCenter.current().add(request)
 
                            
@@ -116,24 +112,25 @@ extension LocationManager: CLLocationManagerDelegate {
                 
             }
             .resume()
-            //send notification
             
             alreadyBreached=true
         }else if (location.distance(from: homeLocation) < distanceThreshold && alreadyBreached){
+            //when you re enter the barrier after leaving it
             alreadyBreached=false
         }
     }
+    
+    //when we pause location updates
     func locationManagerDidPauseLocationUpdates(_ manager: CLLocationManager) {
         print("started monitoring significant")
+        //start looking for big changes in background
         manager.startMonitoringSignificantLocationChanges()
     }
     
+    //when we resume location updates
     func locationManagerDidResumeLocationUpdates(_ manager: CLLocationManager) {
         manager.stopMonitoringSignificantLocationChanges()
+        //stop looking for big changes in background
         print("stopped monitoring significant")
     }
-    
-    
-    
-    
 }
